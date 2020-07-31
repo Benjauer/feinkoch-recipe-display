4699c8303516c32512e836b68c944e89cf7edff2
Benjamin Auer <ben.j.auer@protonmail.com>
2020-07-31 11:45:36 +0200

    add ingredient wrapper props

--- components/IngredientWrapper/IngredientWrapper.tsx
+++ components/IngredientWrapper/IngredientWrapper.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import styles from './IngredientWrapper.module.scss';
 
+interface IngredientWrapperProps {
+    recipeArray: ({ name: string; amount: string; unit: string; image: string; description: string; } | { name: string; amount: null; unit: null; image: string; description: string; })[]
+}
 
-const IngredientWrapper: React.FC = () => {
+
+const IngredientWrapper: React.FC <IngredientWrapperProps> = () => {
 
     return <div>IngredientWrapper</div>
 }
