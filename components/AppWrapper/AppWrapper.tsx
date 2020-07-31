669d85550c3bb8ed9648cf4dd59e6e39759a05d1
Benjamin Auer <ben.j.auer@protonmail.com>
2020-07-31 12:32:59 +0200

    split recipe array

--- components/AppWrapper/AppWrapper.tsx
+++ components/AppWrapper/AppWrapper.tsx
@@ -8,19 +8,29 @@ const AppWrapper: React.FC = () => {
 
     const recipeArray = [
         {
-            name: "Sushireis", 
-            amount: "100", 
-            unit: "g", 
+            name: "Sushireis",
+            amount: "100",
+            unit: "g",
             image: "https://files.billa.at/files/artikel/00-426270_01__1200x1200.jpg",
-            description: "You can use this to make sushi!"}, 
-            
-            {name: "Erdnüsse", 
-            amount: null, 
-            unit: null, 
-            image: "https://files.billa.at/files/artikel/00-373552_01__1200x1200.jpg", 
+            description: "You can use this to make sushi!"
+        },
+
+        {
+            name: "Erdnüsse",
+            amount: null,
+            unit: null,
+            image: "https://files.billa.at/files/artikel/00-373552_01__1200x1200.jpg",
             description: "I had an ex-girlfriend that was allergic to these so I spent years without eating them."
         }
     ]
+
+    // split array in recipe and ingredients
+
+    const recipe = recipeArray[0]
+
+    recipeArray.shift()
+
+    const ingredients = recipeArray
 
 
     return (
@@ -31,8 +41,9 @@ const AppWrapper: React.FC = () => {
             </div>
 
             <div>
-                <IngredientWrapper recipeArray={recipeArray} />
+                <IngredientWrapper recipe={recipe} ingredients={ingredients} />
             </div>
+
         </div>
     )
 }
