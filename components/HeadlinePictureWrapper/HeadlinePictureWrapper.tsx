60c7da99390bc4611af6b805a34591567f786dd6
Benjamin Auer <ben.j.auer@protonmail.com>
2020-07-31 11:30:04 +0200

    add wrapper components

---
+++ components/HeadlinePictureWrapper/HeadlinePictureWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './HeadlinePictureWrapper.module.scss';
+
+const HeadlinePictureWrapper: React.FC = () => {
+
+    return <div>HeadlinePictureWrapper</div>
+}
+
+export default HeadlinePictureWrapper;
---
+++ components/IngredientWrapper/IngredientWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './IngredientWrapper.module.scss';
+
+
+const IngredientWrapper: React.FC = () => {
+
+    return <div>IngredientWrapper</div>
+}
+
+export default IngredientWrapper;
